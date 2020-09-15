32905f60c78aefcd44ab6a5598c581148e308043
Tammy Cuthbert <tammycuthbert427@gmail.com>
2020-09-14 22:24:40 -0500

    update serviceWorker.js

---
+++ Develop/public/serviceWorker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+const FILES_TO_CACHE = ["/", '/icons/icon192x192.png', '/icons/icon-512x512.png', '/dataBase.js', '/index.html', '/index.js', '/styles.css'];
+
+// install
+self.addEventListener("install", function(evt) {
+
+    // pre cache all static assets
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
+});
+
+
+// fetch
+self.addEventListener("fetch", function(evt) {
+    if (evt.request.url.includes("/api/")) {
+        evt.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(evt.request)
+                    .then(response => {
+                        // If the response was good, clone it and store it in the cache.
+                        if (response.status === 200) {
+                            cache.put(evt.request.url, response.clone());
+                        }
+
+                        return response;
+                    })
+                    .catch(err => {
+                        // Network request failed, try to get it from the cache.
+                        return cache.match(evt.request);
+                    });
+            }).catch(err => console.log(err))
+        );
+
+        return;
+    }
+
+    evt.respondWith(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.match(evt.request).then(response => {
+                return response || fetch(evt.request);
+            });
+        })
+    );
+});
